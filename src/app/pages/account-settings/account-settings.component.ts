2dea148432824c2e85022e967093ca9bb149084c
Mariajose Botero <mjoseb@summit.com.co>
2021-08-27 14:38:59 -0500

    Cambiar tema dinámicamente

---
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-settings',
+  templateUrl: './account-settings.component.html',
+  styles: [
+  ]
+})
+export class AccountSettingsComponent implements OnInit {
+  public linkTheme = document.querySelector('#theme');
+  public links: NodeListOf<Element>;
+  constructor() { 
+    this.links = document.querySelectorAll('.selector');
+  }
+
+  ngOnInit(): void {
+    
+    this.checkCurrentTheme();
+  }
+
+  changeTheme(theme: string) {
+    const url = `./assets/css/colors/${theme}.css`;
+    this.linkTheme?.setAttribute('href', url);
+    localStorage.setItem('theme', url);
+
+    this.checkCurrentTheme();
+  }
+
+  checkCurrentTheme() {
+    const links = document.querySelectorAll('.selector');
+    links.forEach(elem => {
+      elem.classList.remove('working');
+      const btnTheme = elem.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+      const currentTheme = this.linkTheme?.getAttribute('href');
+
+      if (btnThemeUrl === currentTheme) {
+        elem.classList.add('working');
+      }
+    });
+
+
+  }
+
+}
