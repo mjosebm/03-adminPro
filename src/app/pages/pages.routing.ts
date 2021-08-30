63c59a676afc27d40cb54a813a6b300cf2701be4
Mariajose Botero <mjoseb@summit.com.co>
2021-08-30 15:32:18 -0500

    BreadCrumbs Listo!

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -16,12 +16,12 @@ const routes: Routes = [
         path: 'dashboard',
         component: PagesComponent,
         children: [
-            { path: '', component: DashboardComponent },
-            { path: 'progress', component: ProgressComponent },
-            { path: 'grafica1', component: Grafica1Component },
-            { path: 'account-settings', component: AccountSettingsComponent },
-            { path: 'promesas', component: PromesasComponent },
-            { path: 'rxjs', component: RxjsComponent },
+            { path: '', component: DashboardComponent, data: { titulo: 'Dashboard'}},
+            { path: 'progress', component: ProgressComponent, data: { titulo: 'Progress'}},
+            { path: 'grafica1', component: Grafica1Component, data: { titulo: 'Grafica'} },
+            { path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Tema'} },
+            { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesa'} },
+            { path: 'rxjs', component: RxjsComponent, data: { titulo: 'Rxjs'} },
         ]
     },
 
--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,11 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnDestroy{
 
-  constructor() { }
+  public titulo: string;
+  public tituloSubs$: Subscription;
 
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    this.titulo = 'Sin Título';
+
+    this.tituloSubs$ = this.getDataRuta().subscribe(({ titulo }) => {   //Desestructurar... Extraer la propiedad titulo del argumento que estoy obteniendo del subscribe
+      this.titulo = titulo;
+      document.title = `AdminPro - ${titulo}`;
+    });
+   
+  }
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
+  }
+
+  getDataRuta(){
+    
+    return this.router.events.pipe(
+      filter((event: any) => event instanceof ActivationEnd),
+      filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+      map((event: ActivationEnd) => event.snapshot.data),
+    )
+
   }
 
 }
